2fb8238fe170fc25653a81606684c8565628b041
panosgeorgakopoulos <pangegeorg@gmail.com>
2025-12-05 17:47:35 +0200

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  // IMPORTANT: Replace 'panos_portfolio3' with your EXACT repository name
+  base: '/panos_portfolio3/', 
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
