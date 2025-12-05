85c840400f2b4ec75afe39d63be3c601c206225c
panosgeorgakopoulos <pangegeorg@gmail.com>
2025-12-05 21:16:18 +0200

    publishing

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // IMPORTANT: Replace 'panos_portfolio3' with your EXACT repository name
-  base: '/panos_portfolio3/', 
+  base: '/', 
   plugins: [react()],
   resolve: {
     alias: {
